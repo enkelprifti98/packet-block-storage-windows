679043bc723d2c6c21862c17467e59f9c2b98cee
Enkel Prifti <43306174+enkelprifti98@users.noreply.github.com>
2020-01-25 15:48:52 -0500

    Update packet-block-storage-detach-windows.ps1

--- packet-block-storage-detach-windows.ps1
+++ packet-block-storage-detach-windows.ps1
@@ -63,12 +63,12 @@ For ($i=0; $i -lt ($Discovered_Iscsi_Targets | Measure-Object).count; $i++) {
         }
     }
     if (($Discovered_Iscsi_Targets[$i].isconnected) -eq $false) {
-        write-output "Volume $($($instance_metadata.volumes[$matching_index].name) is already disconnected."
+        write-output "Volume $($($instance_metadata.volumes[$matching_index].name)) is already disconnected."
     }
     else {
-        write-output "Disconnecting volume $($($instance_metadata.volumes[$matching_index].iqn) / $($instance_metadata.volumes[$matching_index].name)"
+        write-output "Disconnecting volume $($($instance_metadata.volumes[$matching_index].iqn) / $($instance_metadata.volumes[$matching_index].name))"
         Disconnect-IscsiTarget -NodeAddress $Discovered_Iscsi_Targets[$i].NodeAddress
-        write-output "Volume $($($instance_metadata.volumes[$matching_index].iqn) / $($instance_metadata.volumes[$matching_index].name) is now disconnected."
+        write-output "Volume $($($instance_metadata.volumes[$matching_index].iqn) / $($instance_metadata.volumes[$matching_index].name)) is now disconnected."
     }
 }
 
